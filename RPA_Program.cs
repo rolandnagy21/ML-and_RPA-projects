9a2bd45e4452ba4c57f58b9dd2620fea6057f52f
rolandnagy21 <111008540+rolandnagy21@users.noreply.github.com>
2022-08-31 17:44:50 +0200

    Add files via upload

---
+++ RPA_Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace RPAprojectFW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inicializálás
+            string url = args[0];
+            string searchWord = args[1];
+            string workingDirectory;
+            if (args.Length < 3)
+            {
+                workingDirectory = null;
+            }
+            else
+            {
+                workingDirectory = args[2];
+            }
+
+            HasznaltalmaAPI api = new HasznaltalmaAPI();
+
+            api.StartSession(url, searchWord, workingDirectory);
+
+            List<Telefon> telefonok = api.DownloadFirstPage();
+
+
+            List<Telefon> ujTelefonok = api.CompareToDatabase(telefonok);
+            api.AddTelefonsToDatabase(ujTelefonok);
+            api.ExportToExcel(ujTelefonok);
+
+            
+
+        }
+
+    }
+}
